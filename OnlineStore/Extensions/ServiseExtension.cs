f9784a1084193bc3fa111196c3786e4f6851c3dc
YarikVor <yarikhelov@gmail.com>
2023-07-19 22:16:21 +0300

    refactor(identity): change password length

--- OnlineStore/Extensions/ServiseExtension.cs
+++ OnlineStore/Extensions/ServiseExtension.cs
@@ -60,20 +60,16 @@ public static class ServiseExtension
     {
         return services.Configure<IdentityOptions>(options =>
         {
-            // Password settings.
             options.Password.RequireDigit = true;
             options.Password.RequireLowercase = true;
             options.Password.RequireNonAlphanumeric = true;
             options.Password.RequireUppercase = true;
-            options.Password.RequiredLength = 6;
+            options.Password.RequiredLength = 8;
             options.Password.RequiredUniqueChars = 1;
 
-            // Lockout settings.
             options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
             options.Lockout.MaxFailedAccessAttempts = 5;
             options.Lockout.AllowedForNewUsers = true;
-
-            // User settings.
             options.User.AllowedUserNameCharacters =
                 "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
             options.User.RequireUniqueEmail = true;
